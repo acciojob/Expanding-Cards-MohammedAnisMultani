09a3feef0ef2d8d67091f7d7fba8d359cb887b3b
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-11 17:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function expandCard(panelIndex) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach((panel, index) => {
+      if (index + 1 === panelIndex) {
+        panel.classList.add('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    });
+  }
